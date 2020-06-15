31bca646b95f9f86297fae3997293d0f02ea764c
Luis Jorge <jorge.luis.fernando@hotmail.com>
2020-06-15 20:45:04 -0300

    Alterei o arquivo index.py e criei o arquivo sobre.py

--- public_python/index.py
+++ public_python/index.py
@@ -1,3 +1,4 @@
 a = int(input('Escreva um número inteiro: '))
 print('O número digitado foi: {}'.format(a))
 print('O dobro deste número é: {}'.format(2*a))
+print('O triplo deste número é: {}'.format(3*a))
