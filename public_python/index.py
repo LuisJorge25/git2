f00830e57765c68ee37d01ce1db9279347f70650
Luis Jorge <jorge.luis.fernando@hotmail.com>
2020-06-15 20:53:58 -0300

    Alterei index.py, exclui style.py e criei nome.py

--- public_python/index.py
+++ public_python/index.py
@@ -1,3 +1,4 @@
 a = int(input('Escreva um número inteiro: '))
 print('O número digitado foi: {}'.format(a))
 print('O dobro deste número é: {}'.format(2*a))
+print('O raiz quadrada deste número é: {}'.format(a**(1/2)))
---
+++ public_python/scripts/nome.py
@@ -0,0 +1,2 @@
+nome = input('Qual seu nome: ')
+print('Olá, {}!'.format(nome))
